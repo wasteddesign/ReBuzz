367b51c4c770cb04e49deb3c2f3e2ea558a1b1e1
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2024-12-07 23:44:05 +0100

    More assertions

--- ReBuzzTests/UnitTest1.cs
+++ ReBuzzTests/UnitTest1.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
 using System.Windows;
+using System.Windows.Media;
 using AtmaFileSystem;
 using BuzzGUI.Common;
 using BuzzGUI.Interfaces;
@@ -16,6 +18,51 @@ namespace ReBuzzTests;
 
 public class Tests
 {
+  private Dictionary<string, Color> colorDictionary = new Dictionary<string, Color>
+  {
+    { "DC BG", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }, { "DC Text", Color.FromArgb(0xFF, 0xC0, 0xC0, 0xC0) },
+    { "IV BG", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }, { "IV Text", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Amp BG", Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF) },
+    { "MV Amp Handle", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }, { "MV Arrow", Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF) },
+    { "MV Background", Color.FromArgb(0xFF, 0xDA, 0xD6, 0xC9) },
+    { "MV Control", Color.FromArgb(0xFF, 0xAD, 0xAD, 0xAD) }, { "MV Effect", Color.FromArgb(0xFF, 0xC7, 0xAD, 0xA9) },
+    { "MV Effect LED Border", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Effect LED Off", Color.FromArgb(0xFF, 0x64, 0x1E, 0x1E) },
+    { "MV Effect LED On", Color.FromArgb(0xFF, 0xFF, 0x64, 0x64) },
+    { "MV Effect Mute", Color.FromArgb(0xFF, 0x9F, 0x8A, 0x87) },
+    { "MV Effect Pan BG", Color.FromArgb(0xFF, 0x92, 0x65, 0x5F) },
+    { "MV Generator", Color.FromArgb(0xFF, 0xA9, 0xAE, 0xC7) },
+    { "MV Generator LED Border", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Generator LED Off", Color.FromArgb(0xFF, 0x28, 0x28, 0x8C) },
+    { "MV Generator LED On", Color.FromArgb(0xFF, 0x64, 0x64, 0xFF) },
+    { "MV Generator Mute", Color.FromArgb(0xFF, 0x87, 0x8B, 0x9F) },
+    { "MV Generator Pan BG", Color.FromArgb(0xFF, 0x5F, 0x67, 0x92) },
+    { "MV Line", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Machine Border", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Machine Select 1", Color.FromArgb(0xFF, 0xFF, 0xFF, 0xFF) },
+    { "MV Machine Select 2", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Machine Text", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Master", Color.FromArgb(0xFF, 0xC6, 0xBE, 0xAA) },
+    { "MV Master LED Border", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "MV Master LED Off", Color.FromArgb(0xFF, 0x59, 0x59, 0x22) },
+    { "MV Master LED On", Color.FromArgb(0xFF, 0xE8, 0xE8, 0xC1) },
+    { "MV Pan Handle", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }, { "PE BG", Color.FromArgb(0xFF, 0xDA, 0xD6, 0xC9) },
+    { "PE BG Dark", Color.FromArgb(0xFF, 0xBD, 0xB5, 0x9F) },
+    { "PE BG Very Dark", Color.FromArgb(0xFF, 0x9F, 0x93, 0x73) },
+    { "PE Sel BG", Color.FromArgb(0xFF, 0xF7, 0xF7, 0xF4) }, { "PE Text", Color.FromArgb(0xFF, 0x30, 0x30, 0x21) },
+    { "SA Amp BG", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }, { "SA Amp Line", Color.FromArgb(0xFF, 0x00, 0xC8, 0x00) },
+    { "SA Freq BG", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "SA Freq Line", Color.FromArgb(0xFF, 0x00, 0xC8, 0x00) }, { "SE BG", Color.FromArgb(0xFF, 0xDA, 0xD6, 0xC9) },
+    { "SE BG Dark", Color.FromArgb(0xFF, 0xBD, 0xB5, 0x9F) },
+    { "SE BG Very Dark", Color.FromArgb(0xFF, 0x9F, 0x93, 0x73) },
+    { "SE Break Box", Color.FromArgb(0xFF, 0xE0, 0xB0, 0x6D) }, { "SE Line", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) },
+    { "SE Mute Box", Color.FromArgb(0xFF, 0xDD, 0x81, 0x6C) },
+    { "SE Pattern Box", Color.FromArgb(0xFF, 0xC6, 0xBE, 0xA9) },
+    { "SE Sel BG", Color.FromArgb(0xFF, 0xF7, 0xF7, 0xF4) },
+    { "SE Song Position", Color.FromArgb(0xFF, 0xFF, 0xFF, 0x00) },
+    { "SE Text", Color.FromArgb(0xFF, 0x30, 0x30, 0x21) }, { "black", Color.FromArgb(0xFF, 0x00, 0x00, 0x00) }
+  };
+
   [Test]
   public void ReadsGearFilesOnCreation()
   {
@@ -85,151 +132,19 @@ public class Tests
     rebuzzCore.SongCore.Machines.Should().HaveCount(1);
     rebuzzCore.SongCore.MachinesList.Should().Equal(rebuzzCore.SongCore.Machines.Cast<MachineCore>());
 
-    rebuzzCore.SongCore.Machines[0].Name.Should().Be("Master");
-    rebuzzCore.SongCore.Machines[0].Attributes.Should().BeEmpty();
-    rebuzzCore.SongCore.Machines[0].BaseOctave.Should().Be(4);
-    rebuzzCore.SongCore.Machines[0].Commands.Should().BeEmpty();
-    rebuzzCore.SongCore.Machines[0].Data.Should().BeNull();
-    rebuzzCore.SongCore.Machines[0].EnvelopeNames.Should().BeEmpty();
-    rebuzzCore.SongCore.Machines[0].HasStereoInput.Should().BeTrue();
-    rebuzzCore.SongCore.Machines[0].HasStereoOutput.Should().BeTrue();
-    rebuzzCore.SongCore.Machines[0].Graph.Should().Be(rebuzzCore.SongCore);
-    rebuzzCore.SongCore.Machines[0].InputChannelCount.Should().Be(1);
-    rebuzzCore.SongCore.Machines[0].OutputChannelCount.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].Inputs.Should().BeEmpty();
-    rebuzzCore.SongCore.Machines[0].IsActive.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].IsBypassed.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].IsControlMachine.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].IsMuted.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].IsSoloed.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].IsWireless.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].TrackCount.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].LastEngineThread.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].Latency.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].MIDIInputChannel.Should().Be(-1);
-    rebuzzCore.SongCore.Machines[0].Outputs.Should().BeEmpty();
-    rebuzzCore.SongCore.Machines[0].OverrideLatency.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].OversampleFactor.Should().Be(1);
-
-    rebuzzCore.SongCore.Machines[0].ParameterGroups.Should().HaveCount(3);
-    rebuzzCore.SongCore.Machines[0].ParameterGroups[0].Machine.Should().Be(rebuzzCore.SongCore.Machines[0]);
-    rebuzzCore.SongCore.Machines[0].ParameterGroups[0].TrackCount.Should().Be(0);
-
-    rebuzzCore.SongCore.Machines[0].ParameterGroups[0].Parameters.Should().HaveCount(2);
-    AssertParameter(
-      parameter: rebuzzCore.SongCore.Machines[0].ParameterGroups[0].Parameters[0],
-      expectedName: "Amp",
-      expectedDescription: "Amp (0=0%, 4000=100%, FFFE=~400%)",
-      expectedFlags: ParameterFlags.State,
-      expectedDefault: 16384,
-      expectedParentGroup: rebuzzCore.SongCore.Machines[0].ParameterGroups[0],
-      expectedIndexInGroup: 0,
-      expectedMinValue: 0,
-      expectedMaxValue: ushort.MaxValue - 1,
-      expectedNoValue: 0,
-      expectedType: ParameterType.Word);
-    AssertParameter(
-      parameter: rebuzzCore.SongCore.Machines[0].ParameterGroups[0].Parameters[1],
-      expectedName: "Pan",
-      expectedDescription: "Pan (0=Left, 4000=Center, 8000=Right)",
-      expectedFlags: ParameterFlags.State,
-      expectedDefault: 16384,
-      expectedParentGroup: rebuzzCore.SongCore.Machines[0].ParameterGroups[0],
-      expectedIndexInGroup: 1,
-      expectedMinValue: 0,
-      expectedMaxValue: short.MaxValue + 1,
-      expectedNoValue: 0,
-      expectedType: ParameterType.Word);
-
-
-    //TODO:
-    var secondGroupParameters = rebuzzCore.SongCore.Machines[0].ParameterGroups[1].Parameters;
-    secondGroupParameters.Should().HaveCount(3);
-
-    AssertParameter(
-      parameter: secondGroupParameters[0],
-      expectedName: "Volume",
-      expectedDescription: "Master Volume (0=0 dB, 4000=-80 dB)",
-      expectedFlags: ParameterFlags.State,
-      expectedDefault: 0,
-      expectedParentGroup: rebuzzCore.SongCore.Machines[0].ParameterGroups[1],
-      expectedIndexInGroup: 0,
-      expectedMinValue: 0,
-      expectedMaxValue: 16384,
-      expectedNoValue: ushort.MaxValue,
-      expectedType: ParameterType.Word);
-
-    AssertParameter(
-      parameter: secondGroupParameters[1],
-      expectedName: "BPM",
-      expectedDescription: "Beats Per Minute (10-200 hex)",
-      expectedFlags: ParameterFlags.State,
-      expectedDefault: 126,
-      expectedParentGroup: rebuzzCore.SongCore.Machines[0].ParameterGroups[1],
-      expectedIndexInGroup: 1,
-      expectedMinValue: 10,
-      expectedMaxValue: 512,
-      expectedNoValue: 65535,
-      expectedType: ParameterType.Word);
-
-    AssertParameter(
-      parameter: secondGroupParameters[2],
-      expectedName: "TPB",
-      expectedDescription: "Ticks Per Beat (1-20 hex)",
-      expectedFlags: ParameterFlags.State,
-      expectedDefault: 4,
-      expectedParentGroup: rebuzzCore.SongCore.Machines[0].ParameterGroups[1],
-      expectedIndexInGroup: 2,
-      expectedMinValue: 1,
-      expectedMaxValue: 32,
-      expectedNoValue: 255,
-      expectedType: ParameterType.Byte);
-
-    rebuzzCore.SongCore.Machines[0].Patterns.Should().BeEmpty();
-    rebuzzCore.SongCore.Machines[0].PerformanceData.CycleCount.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].PerformanceData.MaxEngineLockTime.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].PerformanceData.PerformanceCount.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].PerformanceData.SampleCount.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].Position.Item1.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].Position.Item2.Should().Be(0);
-    rebuzzCore.SongCore.Machines[0].ManagedMachine.Should().BeNull();
-    rebuzzCore.SongCore.Machines[0].PatternEditorDLL.Should().Be(rebuzzCore.SongCore.Machines[0].DLL);
-    rebuzzCore.SongCore.Machines[0].DLL.Buzz.Should().Be(rebuzzCore);
-    rebuzzCore.SongCore.Machines[0].DLL.Info.Should().BeEquivalentTo(new MachineInfo
-    {
-
-      Author = "WDE",
-      Flags = MachineInfoFlags.NO_OUTPUT,
-      InternalVersion = 100,
-      MaxTracks = 0,
-      MinTracks = 0,
-      Name = "Master",
-      ShortName = "Master",
-      Type = MachineType.Master,
-      Version = 100
-    });
-    rebuzzCore.SongCore.Machines[0].DLL.IsCrashed.Should().BeFalse();
-
-    rebuzzCore.SongCore.Machines[0].DLL.IsLoaded.Should().BeTrue();
-    rebuzzCore.SongCore.Machines[0].DLL.IsMissing.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].DLL.IsOutOfProcess.Should().BeFalse();
-    rebuzzCore.SongCore.Machines[0].DLL.IsManaged.Should().BeTrue();
-    rebuzzCore.SongCore.Machines[0].DLL.Path.Should().Be(AbsoluteDirectoryPath.OfExecutingAssembly().AddDirectoryName("Gear").ToString());
-    rebuzzCore.SongCore.Machines[0].DLL.Presets.Should().BeNull();
-    rebuzzCore.SongCore.Machines[0].DLL.SHA1Hash.Should().BeNull();
-
-
+    AssertIsMasterMachine(rebuzzCore.SongCore.Machines[0], rebuzzCore);
 
     rebuzzCore.Song.Should().BeSameAs(rebuzzCore.SongCore);
 
-    //TODO
-    //bug
-
-    
     // Additional assertions for ReBuzzCore properties
-    rebuzzCore.ThemeColors.Should().BeNull();
-    rebuzzCore.MachineIndex.Should().BeNull();
-    rebuzzCore.MIDIFocusMachine.Should().BeNull();
+    
+    foreach (var (key, value) in colorDictionary)
+    {
+      rebuzzCore.ThemeColors[key].Should().Be(value);
+    }
+
+    rebuzzCore.MachineIndex.Should().BeEquivalentTo(new MenuItemCore());
+    rebuzzCore.MIDIFocusMachine.Should().Be(rebuzzCore.SongCore.MachinesList[0]);
     rebuzzCore.MIDIFocusLocked.Should().BeFalse();
     rebuzzCore.MIDIActivity.Should().BeFalse();
     rebuzzCore.IsPianoKeyboardVisible.Should().BeFalse();
@@ -237,11 +152,19 @@ public class Tests
     rebuzzCore.IsCPUMonitorWindowVisible.Should().BeFalse();
     rebuzzCore.IsHardDiskRecorderWindowVisible.Should().BeFalse();
     rebuzzCore.IsFullScreen.Should().BeFalse();
-    rebuzzCore.MachineDLLsList.Should().BeEmpty();
     rebuzzCore.Instruments.Should().BeEmpty();
-    rebuzzCore.AudioDrivers.Should().BeEmpty();
+    rebuzzCore.AudioDrivers.Should().NotBeEmpty();
     rebuzzCore.SelectedAudioDriver.Should().BeNullOrEmpty();
-    rebuzzCore.SelectedAudioDriverSampleRate.Should().Be(0);
+    rebuzzCore.SelectedAudioDriverSampleRate.Should().Be(44100);
+    rebuzzCore.MachineDLLsList.Should().HaveCount(1);
+    var modernPatternEditor = rebuzzCore.MachineDLLsList["Modern Pattern Editor"];
+    modernPatternEditor.Buzz.Should().Be(rebuzzCore);
+    modernPatternEditor.Path.Should().Be(AbsoluteDirectoryPath.OfExecutingAssembly().AddFileName("StubMachine.dll").ToString()); //bug parameterize
+    modernPatternEditor.Info.Name.Should().Be(FakeModernPatternEditor.GetMachineDecl().Name);
+    modernPatternEditor.Info.Author.Should().Be(FakeModernPatternEditor.GetMachineDecl().Author);
+    modernPatternEditor.Info.Flags.Should().Be(MachineInfoFlags.STEREO_EFFECT | MachineInfoFlags.LOAD_DATA_RUNTIME);
+    //bug more assertions and take some of this from the fake machine scanner.
+
     //
     //// Assertions for static properties
     ReBuzzCore.buildNumber.Should().Be(0);
@@ -256,6 +179,142 @@ public class Tests
     ReBuzzCore.GlobalState.StateFlags.Should().Be(0);
     ReBuzzCore.GlobalState.MIDIFiltering.Should().Be(0);
     ReBuzzCore.GlobalState.SongClosing.Should().Be(0);
+  }
+
+  private static void AssertIsMasterMachine(IMachine machine, ReBuzzCore rebuzzCore)
+  {
+    machine.Name.Should().Be("Master");
+    machine.Attributes.Should().BeEmpty();
+    machine.BaseOctave.Should().Be(4);
+    machine.Commands.Should().BeEmpty();
+    machine.Data.Should().BeNull();
+    machine.EnvelopeNames.Should().BeEmpty();
+    machine.HasStereoInput.Should().BeTrue();
+    machine.HasStereoOutput.Should().BeTrue();
+    machine.Graph.Should().Be(rebuzzCore.SongCore);
+    machine.InputChannelCount.Should().Be(1);
+    machine.OutputChannelCount.Should().Be(0);
+    machine.Inputs.Should().BeEmpty();
+    machine.IsActive.Should().BeFalse();
+    machine.IsBypassed.Should().BeFalse();
+    machine.IsControlMachine.Should().BeFalse();
+    machine.IsMuted.Should().BeFalse();
+    machine.IsSoloed.Should().BeFalse();
+    machine.IsWireless.Should().BeFalse();
+    machine.TrackCount.Should().Be(0);
+    machine.LastEngineThread.Should().Be(0);
+    machine.Latency.Should().Be(0);
+    machine.MIDIInputChannel.Should().Be(-1);
+    machine.Outputs.Should().BeEmpty();
+    machine.OverrideLatency.Should().Be(0);
+    machine.OversampleFactor.Should().Be(1);
+
+    machine.ParameterGroups.Should().HaveCount(3);
+    machine.ParameterGroups[0].Machine.Should().Be(machine);
+    machine.ParameterGroups[0].TrackCount.Should().Be(0);
+
+    machine.ParameterGroups[0].Parameters.Should().HaveCount(2);
+    AssertParameter(
+      parameter: machine.ParameterGroups[0].Parameters[0],
+      expectedName: "Amp",
+      expectedDescription: "Amp (0=0%, 4000=100%, FFFE=~400%)",
+      expectedFlags: ParameterFlags.State,
+      expectedDefault: 16384,
+      expectedParentGroup: machine.ParameterGroups[0],
+      expectedIndexInGroup: 0,
+      expectedMinValue: 0,
+      expectedMaxValue: ushort.MaxValue - 1,
+      expectedNoValue: 0,
+      expectedType: ParameterType.Word);
+    AssertParameter(
+      parameter: machine.ParameterGroups[0].Parameters[1],
+      expectedName: "Pan",
+      expectedDescription: "Pan (0=Left, 4000=Center, 8000=Right)",
+      expectedFlags: ParameterFlags.State,
+      expectedDefault: 16384,
+      expectedParentGroup: machine.ParameterGroups[0],
+      expectedIndexInGroup: 1,
+      expectedMinValue: 0,
+      expectedMaxValue: short.MaxValue + 1,
+      expectedNoValue: 0,
+      expectedType: ParameterType.Word);
+
+
+    //TODO:
+    var secondGroupParameters = machine.ParameterGroups[1].Parameters;
+    secondGroupParameters.Should().HaveCount(3);
+
+    AssertParameter(
+      parameter: secondGroupParameters[0],
+      expectedName: "Volume",
+      expectedDescription: "Master Volume (0=0 dB, 4000=-80 dB)",
+      expectedFlags: ParameterFlags.State,
+      expectedDefault: 0,
+      expectedParentGroup: machine.ParameterGroups[1],
+      expectedIndexInGroup: 0,
+      expectedMinValue: 0,
+      expectedMaxValue: 16384,
+      expectedNoValue: ushort.MaxValue,
+      expectedType: ParameterType.Word);
+
+    AssertParameter(
+      parameter: secondGroupParameters[1],
+      expectedName: "BPM",
+      expectedDescription: "Beats Per Minute (10-200 hex)",
+      expectedFlags: ParameterFlags.State,
+      expectedDefault: 126,
+      expectedParentGroup: machine.ParameterGroups[1],
+      expectedIndexInGroup: 1,
+      expectedMinValue: 10,
+      expectedMaxValue: 512,
+      expectedNoValue: 65535,
+      expectedType: ParameterType.Word);
+
+    AssertParameter(
+      parameter: secondGroupParameters[2],
+      expectedName: "TPB",
+      expectedDescription: "Ticks Per Beat (1-20 hex)",
+      expectedFlags: ParameterFlags.State,
+      expectedDefault: 4,
+      expectedParentGroup: machine.ParameterGroups[1],
+      expectedIndexInGroup: 2,
+      expectedMinValue: 1,
+      expectedMaxValue: 32,
+      expectedNoValue: 255,
+      expectedType: ParameterType.Byte);
+
+    machine.Patterns.Should().BeEmpty();
+    machine.PerformanceData.CycleCount.Should().Be(0);
+    machine.PerformanceData.MaxEngineLockTime.Should().Be(0);
+    machine.PerformanceData.PerformanceCount.Should().Be(0);
+    machine.PerformanceData.SampleCount.Should().Be(0);
+    machine.Position.Item1.Should().Be(0);
+    machine.Position.Item2.Should().Be(0);
+    machine.ManagedMachine.Should().BeNull();
+    machine.PatternEditorDLL.Should().BeNull(); //bug investigate!!
+    machine.DLL.Buzz.Should().Be(rebuzzCore);
+    machine.DLL.Info.Should().BeEquivalentTo(new MachineInfo
+    {
+
+      Author = "WDE",
+      Flags = MachineInfoFlags.NO_OUTPUT,
+      InternalVersion = 100,
+      MaxTracks = 0,
+      MinTracks = 0,
+      Name = "Master",
+      ShortName = "Master",
+      Type = MachineType.Master,
+      Version = 100
+    });
+    machine.DLL.IsCrashed.Should().BeFalse();
+
+    machine.DLL.IsLoaded.Should().BeTrue();
+    machine.DLL.IsMissing.Should().BeFalse();
+    machine.DLL.IsOutOfProcess.Should().BeFalse();
+    machine.DLL.IsManaged.Should().BeTrue();
+    machine.DLL.Path.Should().Be(AbsoluteDirectoryPath.OfExecutingAssembly().AddDirectoryName("Gear").ToString());
+    machine.DLL.Presets.Should().BeNull();
+    machine.DLL.SHA1Hash.Should().BeNull();
   }
 
   private static void AssertParameter(
