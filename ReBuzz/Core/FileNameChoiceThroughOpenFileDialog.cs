2f55809ec9094f6a6a2bdd40394c12d20437157a
Grzegorz Gałęzowski <grzesiek.galezowski@gmail.com>
2025-01-10 23:00:46 +0100

    Addressed the last TODO in this test pack

--- ReBuzz/Core/FileNameChoiceThroughOpenFileDialog.cs
+++ ReBuzz/Core/FileNameChoiceThroughOpenFileDialog.cs
@@ -2,11 +2,6 @@ using Microsoft.Win32;
 
 namespace ReBuzz.Core
 {
-    internal interface IFileNameChoice
-    {
-        ChosenValue<string> SelectFileName();
-    }
-
     internal class FileNameToLoadChoiceThroughOpenFileDialog : IFileNameChoice
     {
         public ChosenValue<string> SelectFileName()
@@ -20,22 +15,6 @@ namespace ReBuzz.Core
             }
 
             return fileName;
-        }
-    }
-
-    internal class FileNameToSaveChoiceThroughSaveFileDialog : IFileNameChoice //bug move and the interface as well
-    {
-        public ChosenValue<string> SelectFileName()
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Songs with waves (*.bmx)|*.bmx|Songs without waves (*.bmw)|*.bmw|ReBuzz XML (*.bmxml)|*.bmxml";
-            ChosenValue<string> saveFileName = ChosenValue<string>.Nothing;
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                saveFileName = ChosenValue<string>.Just(saveFileDialog.FileName);
-            }
-
-            return saveFileName;
         }
     }
 }
