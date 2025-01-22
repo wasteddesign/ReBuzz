b1b9beb9c3fce9df30747936e444b31363946b11
wasteddesign <ilkka.syrjala@outlook.com>
2025-01-22 20:22:09 +0200

    AudioEngine improvements

--- ReBuzz/Audio/CommonAudioProvider.cs
+++ ReBuzz/Audio/CommonAudioProvider.cs
@@ -237,6 +237,7 @@ namespace ReBuzz.Audio
             {
                 multimediaTimer.Stop();
                 multimediaTimer.Dispose();
+                multimediaTimer = null;
             }
         }
 
